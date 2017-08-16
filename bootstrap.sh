03dff25f16c343e7a87ac40c49fb56b86d53cc2f
duangsuse <fedora-opensuse@outlook.com>
2017-08-16 22:10:33 +0800

    version 0.0.8 (first version): - support markdown format fortran with link - support {content} --{author} format - random fortran - serde_json auto deserialize - would panic when argument/data is wrong
    known bugs:
    - does not support TLS

---
+++ bootstrap.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#Script file for deploy naive fortunes
+
+FORT_URL="https://gist.githubusercontent.com/FiveYellowMice/c0581783983b05ca28f7/raw/7217e8b658aa0fe058e1ea742ede612040ed2a37/fortunes.json"
+
+if test -e ./src ;then
+    echo "I: src dir found,invoking cargo to build source..."
+    command cargo 2> /dev/null
+    if [ $? = 127 ] ;then
+        echo "cargo not found,install latest nightly rust toolchain first."
+        exit 1
+    else
+        cargo install --force
+        echo "cargo exited with "$?
+        echo "grabbing fortunes from FiveYellowMice's gist..."
+        command wget 2> /dev/null
+        if [ $? = 127 ] ;then
+            echo "please install wget first ..."
+            exit 1
+        else
+            wget -q $FORT_URL -O ~/fort.json
+        fi 
+        echo "wget exited with"$?
+        echo "preparing cron... (root access needed) (PLEASE INSTALL cronie !!!)"
+        sudo echo "#!/bin/sh" > /etc/cron.daily/fortnaive
+        sudo echo "wget "$FORT_URL" -O "~/fort.json >> /etc/cron.daily/fortnaive
+        sudo chmod +x /etc/cron.daily/fortnaive
+        sudo echo "24 * * * * root systemctl restart fort.service" >> /etc/cron.d/0daily
+        sudo echo "24 * * * * root run-parts /etc/cron.daily" >> /etc/cron.d/0daily
+        cfgfile=`cat /etc/cron.daily/fortnaive`
+        echo "${cfgfile} writed"
+        echo "preparing systemd service (this will panic when running on non-systemd distro)"
+        echo "(root access needed)"
+        echo "\[Unit]
+Description=fortune
+After=network.target
+
+[Service]
+ExecStart=${HOME}/.cargo/bin/naivefortunes ${HOME}/fort.json
+Restart=always
+RestartSec=0" > /lib/systemd/system/fort.service
+       echo "done. use systemctl start fort.service to start."
+    fi
+else
+    echo "please run this script in project dir."
+    exit 1
+fi
---
+++ src/main.rs
@@ -0,0 +1,84 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate serde;
+extern crate serde_json;
+extern crate rocket;
+extern crate rand;
+
+#[macro_use]
+extern crate serde_derive;
+
+use std::env::args;
+use std::fs::File;
+use std::io::Read;
+use rocket::State;
+use rocket::Request;
+use rocket::response::Redirect;
+use rand::Rng;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct Fortune {
+    pub content: String,
+    pub author: Option<String>,
+    pub link: Option<String>,
+}
+
+fn get_random_idx(len: usize) -> usize {
+    Rng::gen_range(&mut rand::thread_rng(), 0, len)
+}
+
+#[get("/body")] //just fetch fortune content
+fn rand_body(state: State<Vec<Fortune>>) -> String {
+    let data = state.to_vec();
+    let data_len = data.len();
+    format!("{}", &data[get_random_idx(data_len)].content)
+}
+
+#[get("/fortune")] //get formated fortune
+fn fortune(state: State<Vec<Fortune>>) -> String {
+    let data = state.to_vec();
+    let data_len = data.len();
+    let data_selected = &data[get_random_idx(data_len)];
+    if let Some(ref l) = data_selected.link {
+        if let Some(ref a) = data_selected.author {
+            format!("[{} --{}]({})", data_selected.content, a, l)
+        } else {
+            format!("[{}]({})", data_selected.content, l)
+        }
+    } else {
+        if let Some(ref a) = data_selected.author {
+            format!("{} --{}", data_selected.content, a)
+        } else {
+            format!("{}", data_selected.content)
+        }
+    }
+}
+
+#[get("/")]
+fn redirect() -> Redirect {
+    Redirect::to("/fortune")
+}
+
+#[error(404)]
+fn not_found(req: &Request) -> String {
+    format!("{} {} ,这是最吼的. #(滑稽)", req.method(), req.uri())
+}
+
+fn main() {
+    let mut prog_args = args();
+    prog_args.next().unwrap();
+    let file_loc = prog_args.next().unwrap_or_else(
+        || panic!("argument required"),
+    );
+    let mut file_str = String::new();
+    if let Ok(mut f) = File::open(file_loc) {
+        f.read_to_string(&mut file_str).unwrap();
+    }
+    let deserialized: Vec<Fortune> = serde_json::from_str(&file_str).unwrap();
+    rocket::ignite()
+        .catch(errors![not_found])
+        .mount("/", routes![rand_body, fortune, redirect])
+        .manage(deserialized)
+        .launch();
+}
